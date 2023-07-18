037d5925c84a48efe02a1d2d7cf72eb9313ba483
joseluistello <jltello96@gmail.com>
2023-07-18 00:05:30 -0600

    opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,12 @@ import HeaderSection from '@/components/Header'
 import FooterSection from '@/components/Footer'
 
 
-
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Helena | Tu compañera de crecimiento personal',
-  description: 'Te acompañamos en tu proceso de crecimiento',
+  description: 'Estaremos contigo en todo momento, incluso en los momentos más difíciles de la vida.'
 }
 
 export default function RootLayout({
@@ -21,6 +21,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+        <head>
+        <meta property="twitter:image" content="https://opengraphs.s3.us-east-2.amazonaws.com/Helena.jpeg" />
+      </head>
       <body className={inter.className}>
       <HeaderSection/>
       {children}
